9336f78326ab3a52c10bb3420c8216ac248029a1
Vaishnavi Nikumbh <vaishnavi.official30@gmail.com>
2024-12-27 13:51:25 +0530

    Hi

--- Patternofstar.java
+++ Patternofstar.java
@@ -1,12 +1,9 @@
-
-
-
 class Patternofstar{
     public static void main(String args[]){
 
-        for (int i = 1 ; i <= 5 ; i++){
+        for (int i = 1 ; i <= 3 ; i++){
             
-           for (int j = 1 ; j <= 4 ; j++){
+           for (int j = 1 ; j <= 3 ; j++){
 
                 System.out.print("*");
             }
